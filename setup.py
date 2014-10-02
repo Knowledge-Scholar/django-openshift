a0572654b9ca9ee1b462ac712f6d10e338ffe9ac
Adam-Williams <Adam-Williams@users.noreply.github.com>
2014-10-01 21:30:39 -0400

    Update setup.py
    Moved requirements to requirements.txt and updated references to appropriate repo

--- setup.py
+++ setup.py
@@ -5,20 +5,15 @@ import os
 # Put here required packages or
 # Uncomment one or more lines below in the install_requires section
 # for the specific client drivers/modules your application needs.
-packages = ['Django<=1.6',
-            'static3',  # If you want serve the static files in the same server
-            # 'mysql-connector-python',
-            # 'pymongo',
-            # 'psycopg2',
-           ]
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
-     packages.append('django-redis-cache')
-     packages.append('hiredis')
+    packages.append('django-redis-cache')
+    packages.append('hiredis')
 
-setup(name='YourAppName', version='1.0',
-      description='OpenShift Python-3.3 / Django-1.6 Community Cartridge based application',
-      author='Your Name', author_email='admin@example.org',
-      url='https://pypi.python.org/pypi',
-      install_requires=packages,
-     )
+setup(name='django 1.7 on Red Hat Openshift',
+    version='0.1',
+    description='Django 1.7 on OpenShift Python 3.3',
+    author='Your name',
+    author_email='example@example.com',
+    url='https://github.com/Knowledge-Scholar/django-py3-openshift-quickstart',
+)
