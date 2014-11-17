33e18ad61ae942cd6eda01ef8a94056459b8f68c
Adam <a.williams.jr.1010@gmail.com>
2014-11-16 22:32:24 -0500

    Added necessary changes to settings, and openshift action hooks. Renamed static folder and moved wsgi to base directory

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,7 +84,9 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
 
 ROOT_URLCONF = 'mysite.urls'
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+OPENSHIFT_PYTHON_WSGI_APPLICATION = os.path.join(BASE_DIR)
+
+WSGI_APPLICATION = os.path.join(BASE_DIR)
 
 TEMPLATE_DIRS = (
      os.path.join(BASE_DIR,'templates'),
@@ -131,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets','static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
